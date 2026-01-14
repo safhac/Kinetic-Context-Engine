0bb033dd2866fe013f99417c5e9b1ccf3d5d1159
safhac <safhac@gmail.com>
2026-01-14 10:02:45 +0200

    removed legacy code and cleared upload dirs

--- app/consumers/shared_lib/kafka_utils.py
+++
@@ -1,8 +0,0 @@
-import json
-from kafka import KafkaProducer
-
-def get_producer(broker_url: str):
-    return KafkaProducer(
-        bootstrap_servers=broker_url,
-        value_serializer=lambda v: json.dumps(v).encode('utf-8')
-    )
